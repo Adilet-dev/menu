8d4df0b5caa848e8c13cda525e701ec1b672cd10
Adilet-dev <arapbaevich20@gmail.com>
2022-11-20 11:35:44 +0300

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 40,
+    slidesPerGroup: 1,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+
+
+
